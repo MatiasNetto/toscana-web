2abe4ceb5e110ef6e46633b77f3e132b599fe134
MatiasNetto <matiasnetto25@gmail.com>
2021-05-30 19:53:27 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch} from "react-router-dom";
 
 //pages
 import TestPage from './pages/TestPage'
@@ -7,9 +7,12 @@ import TestPage from './pages/TestPage'
 function App() {
   return (
     <Router>
+      <Switch>
+
       <Route path='/test' exact children={TestPage}/>
       <Route exact path='/' children='home'/>
 
+      </Switch>
     </Router>
   );
 }
