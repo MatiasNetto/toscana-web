1b5d0d267dd7b42fdb0b50b84e277ba0426f4e40
MatiasNetto <matiasnetto25@gmail.com>
2021-06-01 10:27:09 -0300

    Home slider added with useState hook

---
+++ src/components/HomeSlider.jsx
@@ -0,0 +1,52 @@
+import React, {useState, useEffect} from 'react';
+
+//styles
+import './styles/HomeSlider.css'
+
+//assets
+import logo from '../assets/logos/logo.png'
+import sliderImage1 from '../assets/slider/slider_1.jpg'
+import sliderImage2 from '../assets/slider/slider_2.jpg'
+import sliderImage3 from '../assets/slider/slider_3.jpg'
+import sliderImage4 from '../assets/slider/slider_4.jpg'
+import sliderImage5 from '../assets/slider/slider_5.jpg'
+import sliderImage6 from '../assets/slider/slider_6.jpg'
+
+//components
+import Button from '../components/Button'
+
+
+const HomeSLider = () => {
+
+    const [sliderNumber, setSliderNumber] = useState(0)
+        let sliderImages = [sliderImage1,sliderImage2,sliderImage3,sliderImage4,sliderImage5,sliderImage6]
+        sliderImages[-1] = sliderImages[sliderImages.length -1]
+    
+    useEffect(()=> {
+        console.log(sliderNumber);
+        if (sliderNumber == sliderImages.length) {setSliderNumber(0)}
+        else {setTimeout(()=>{setSliderNumber(sliderNumber + 1)},3000)}
+    })
+
+
+
+    return (
+        <div className='main-container'>
+            <img src={sliderNumber %2 != 0 ? sliderImages[sliderNumber] : sliderImages[sliderNumber - 1]} className={sliderNumber %2 != 0 ? 'slider-shown' : 'slider-hidden'} alt="" />
+            <img src={sliderNumber %2 == 0 ? sliderImages[sliderNumber] : sliderImages[sliderNumber - 1]} className={sliderNumber %2 == 0 ? 'slider-shown' : 'slider-hidden'} alt="" />
+            <img className='logo-img' src={logo} alt="Toscana logo" />
+            <div className='tittles-container'>
+                <h2 className='tittle'>Toscana</h2>
+                <h3 className='subtittle'>Accesorios</h3>
+            </div>
+            <Button text='Ver mas'/>
+        </div> );
+}
+ 
+export default HomeSLider;
+
+
+
+
+
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,73 +1,30 @@
-import React, {useRef, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 //styles
 import './styles/Home.css'
 
-//assets
-import logo from '../assets/logos/logo.png'
-import sliderImage1 from '../assets/slider/slider_1.jpg'
-import sliderImage2 from '../assets/slider/slider_2.jpg'
-import sliderImage3 from '../assets/slider/slider_3.jpg'
-import sliderImage4 from '../assets/slider/slider_4.jpg'
-import sliderImage5 from '../assets/slider/slider_5.jpg'
-import sliderImage6 from '../assets/slider/slider_6.jpg'
 
 //components
 import NavBar from '../components/NavBar'
 import Button from '../components/Button'
 import Subtittle from '../components/Subtittle'
 import CategoryCard from '../components/CategoryCard'
+import HomeSLider from '../components/HomeSlider';
 
+let sliderNumber = 0
 function Home() {
-    const slider1 = useRef(null);
-    const slider2 = useRef(null)
-    let sliderImages = [sliderImage1,sliderImage2,sliderImage3,sliderImage4,sliderImage5,sliderImage6]
-    sliderImages[-1] = sliderImages[sliderImages.length -1]
-    let sliderNumber = 1
     
-    
-    const changeImage = () => {
-        console.log('ashe');
-
-        //change visibility
-        if (sliderNumber %2 == 0) {
-            slider1.current.className = 'slider-shown'
-            slider2.current.className = 'slider-hidden'
-            slider1.current.src = sliderImages[sliderNumber]
-            slider2.current.src = sliderImages[sliderNumber - 1]
-        } else {
-            slider1.current.className = 'slider-hidden'
-            slider2.current.className = 'slider-shown'
-            slider1.current.src = sliderImages[sliderNumber - 1]
-            slider2.current.src = sliderImages[sliderNumber]
-        }
-
-        sliderNumber ++
-
-        //reset counter
-        if (sliderNumber == (sliderImages.length)) {sliderNumber = 0}
-    }
-    setInterval(changeImage,4000)
-
     return ( 
         <>
             <NavBar/>
-            <div className='main-container'>
-                <img ref={slider1} src={sliderImage1} className='slider-shown' alt="" />
-                <img ref={slider2} src={sliderImage1} className='slider-hidden' alt="" />
-                <img className='logo-img' src={logo} alt="Toscana logo" />
-                <div className='tittles-container'>
-                    <h2 className='tittle'>Toscana</h2>
-                    <h3 className='subtittle'>Accesorios</h3>
-                </div>
-                <Button text='Ver mas'/>
-            </div>
-            <div className='products-container'>
+            <HomeSLider/>
+           
+            {/* <div className='products-container'>
                 <Subtittle text='Ver productos'/>
                 <div className='categories-grid-container'>
                     <CategoryCard category='Anillos' img={sliderImage1} position='right'/>
                 </div>
-            </div>
+            </div> */}
 
         
         </>
