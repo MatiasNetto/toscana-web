b0db33ef3e634a521ff79517f7a2101683a00712
nicochavez <nicolaschavez530@gmail.com>
2021-06-02 12:59:24 -0300

    error

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import HamburgerMenu from './HamburgerMenu';
 
 const NavBar = () => {
     return ( 
         <>
-        
+        <HamburgerMenu/>
         </>
      );
 }
